35261b30de8cf7a0b120092da505d9e75c055c1e
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2023-09-20 01:10:52 +0400

    textSpin

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -56,25 +49,13 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
 
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: RichText(text: TextSpan(
+          children: [
+            TextSpan(text: "Pink",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35,color: Colors.pink))
+          ]
+        ))
+      )
+    // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
